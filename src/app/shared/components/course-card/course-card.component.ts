84877cc7e4a34ec18deb72aae6103a2cb30fa7ee
Mary Polish <polishchuk.mariyatv21@gmail.com>
2025-09-15 12:03:02 +0300

    complete tasks

--- src/app/shared/components/course-card/course-card.component.ts
+++ src/app/shared/components/course-card/course-card.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-course-card',
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.scss']
 })
-export class CourseCardComponent {}
+export class CourseCardComponent {
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() creationDate: Date = new Date();
+  @Input() duration: number = 0;
+  @Input() authors: string[] = [];
+  @Input() editable: boolean = false;
+
+  @Output() showCourse: EventEmitter<void> = new EventEmitter<void>();
+  @Output() editCourse: EventEmitter<void> = new EventEmitter<void>();
+  @Output() deleteCourse: EventEmitter<void> = new EventEmitter<void>();
+
+  onShowCourse() {
+    this.showCourse.emit();
+  }
+  
+  onEditCourse() {
+    this.editCourse.emit();
+  }
+  
+  onDeleteCourse() {
+    this.deleteCourse.emit();
+  }
+}
